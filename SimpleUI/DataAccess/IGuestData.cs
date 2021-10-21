d563e9bd30c99e9eea75366fcaac339ea3b9ab6e
Doniyor Niyozov <doniyozov@eaglebusinesssoftware.com>
2021-10-21 19:22:46 +0500

    added Blazor UI to source control

---
+++ SimpleUI/DataAccess/IGuestData.cs
@@ -0,0 +1,25 @@
+﻿using Refit;
+using SimpleUI.Models;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace SimpleUI.DataAccess
+{
+    public interface IGuestData
+    {
+        [Get("/Guests")]
+        Task<List<GuestModel>> GetGuests();
+
+        [Get("/Guests/(id)")]
+        Task<GuestModel> GetGuest(int id);
+
+        [Post("/Guiests")]
+        Task CreateGuest([Body] GuestModel guest);
+
+        [Put("/Guests/{id}")]
+        Task UpdateGeuest(int id, [Body] GuestModel geuest);
+
+        [Delete("/Guests/{id}")]
+        Task DeleteGuest(int id);
+    }
+}
