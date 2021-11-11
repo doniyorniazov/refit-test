1f8834fc251e66dd615dcf589cdfd744e99518d0
Doniyor Niyozov <doniyozov@eaglebusinesssoftware.com>
2021-11-11 10:57:09 +0500

    misc

--- Startup.cs
+++ Startup.cs
@@ -1,16 +1,12 @@
+using Autofac.Core;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Versioning;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace SimpleApi
 {
@@ -37,6 +33,14 @@ namespace SimpleApi
                     .AllowAnyHeader()
                     .AllowAnyMethod();
                 });
+            });
+
+            services.AddApiVersioning(c =>
+            {
+                c.DefaultApiVersion = new ApiVersion(1, 0);
+                c.AssumeDefaultVersionWhenUnspecified = true;
+                c.ReportApiVersions = true;
+                c.ApiVersionReader = new HeaderApiVersionReader("api-version");
             });
 
             services.AddSwaggerGen(c =>
